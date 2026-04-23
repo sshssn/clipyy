aac6162cad297cae8c6773963e7ca710f290edc0
sshssn <sshssn@yahoo.com>
2026-04-23 12:36:36 +0500

    Initial Commit

---
+++ Clipyy/ClipyyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipyyApp.swift
+//  Clipyy
+//
+//  Created by ssh on 23/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ClipyyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
